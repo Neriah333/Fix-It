43f6cc25209d3dd109a560cee77aae50bbe1c3d6
Neriah333 <pheobenyawanda@gmail.com>
2026-01-27 22:41:42 +0300

    added code lines to the server.js and changed the MONGO_URI connection string

--- server/server.js
+++ server/server.js
@@ -12,6 +12,6 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port http://localhost:${PORT}`);
 });
 
