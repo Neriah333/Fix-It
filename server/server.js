771abaf8c75cc524567463b06f93fcb3181c69c0
Neriah333 <pheobenyawanda@gmail.com>
2026-02-02 23:23:54 +0300

    Created controller and route files

--- server/server.js
+++ server/server.js
@@ -10,6 +10,10 @@ connectDB();
 app.use(cors());
 app.use(express.json());
 
+app.get('/', async (req, res) => {
+  res.send('API is running...')
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
